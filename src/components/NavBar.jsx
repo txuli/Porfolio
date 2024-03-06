5ddb0b67f3ea8a534bd6c4f50a66ac675d6f0046
Txuli17 <127785824+Txuli17@users.noreply.github.com>
2024-03-06 20:08:49 +0100

    Routes fixed

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -9,8 +9,8 @@ function App() {
   return (
       <div className='w-full flex justify-center px-1'>
         <div className='bg-zinc-800 w-75 h-16 flex items-center text-center gap-4 mt-5 rounded-md px-4'>
-        <Link to="/estdrytfgbhujnimk" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Home</Link>
-        <a href="/about" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> About me</a>
+        <Link to="/home" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Home</Link>
+        <Link to="/about" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> About me</Link>
         <a href="/projedcts" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Projects</a>
         </div> 
         <Outlet />
