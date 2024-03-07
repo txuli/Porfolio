aeb1dfc90b777da0c922a5621174dcdc1bebe73b
Txuli17 <127785824+Txuli17@users.noreply.github.com>
2024-03-07 19:34:07 +0100

    Main Page
    I added different pages and I develop the home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,13 +7,16 @@ function App() {
   // navigate('/')
 
   return (
-      <div className='w-full flex justify-center px-1'>
-        <div className='bg-zinc-800 w-75 h-16 flex items-center text-center gap-4 mt-5 rounded-md px-4'>
+      <div className='w-full grid grid-cols-3  px-1'>
+        <h1 className='flex w-auto mx-5 items-center mt-5 '>My life programming</h1>
+        <div className='flex items-center justify-center'>
+        <div className='bg-zinc-800 w-auto h-16 flex items-center text-center gap-4 mt-5 rounded-md px-4'>
         <Link to="/home" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Home</Link>
         <Link to="/about" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> About me</Link>
         <a href="/projedcts" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Projects</a>
-        </div> 
+        </div>
         <Outlet />
+    </div>
     </div>
   )
 }
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,21 @@ const Home = () =>{
     return (
         <div className="h-screen">
             <NavBar />
-        hola
+            <div className='flex justify-center'>
+                <div className='bg-zinc-600 flex justify-center items-center w-96 my-9 py-6 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)]'> 
+                    <div className='flex justify-center '>
+                    <img src="src/pages/media/DSC_9703.jpeg" alt="imagen de Asier Alberi" className='h-40 rounded-full'/></div>
+                    <div className='mx-3'>
+                    <div className='justify-center text w-15'>Asier Alberdi</div>
+                    <div className='w-15 text-zinc-400'>@Txuli</div>
+                    <div className='w-7'>
+                        <a href="https://github.com/Txuli17" target='blank'><img src="src/pages/media/github-mark-white.png" alt="github" className='h-7' /></a>
+                    
+                    </div>
+                </div>
+            </div>
+                
+        </div>
         </div>
     );
 }
