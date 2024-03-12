cb1b5a119ebfc3463e6892f184f3a70859dd56a9
Txuli17 <127785824+Txuli17@users.noreply.github.com>
2024-03-12 14:58:13 +0100

    responsive menu img bugs fixed
    I implement the responsive menu and I change "src" to the images so that they can be seen on the hosting.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,32 +1,50 @@
 import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
 import { useNavigate, Link } from 'react-router-dom'
-function App({}) {
+function App({ }) {
   const navigate = useNavigate()
   const [screenWidth, setScreenWidth] = useState(window.innerWidth)
+  const [isOpen, setIsOpen] = useState(false)
 
-  
+  // Comentar en release
+  useEffect(() => {
+    setInterval(() => {
+      setScreenWidth(window.innerWidth)
+    }, 1000)
+  }, [])
 
-
+  const Menu_click = () => {
+    setIsOpen(!isOpen)
+  }
   return (
     <div>
       {screenWidth > 600 ? (
         <div className='w-full grid grid-cols-3 px-1'>
-            <h1 className='flex w-auto mx-5 items-center mt-5 '>My life programming</h1>
-            <div className='flex items-center justify-center'>
-            <div className='bg-zinc-800 w-auto h-16 flex items-center text-center gap-4 mt-5 rounded-md px-4'>
-            <Link to="/home" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Home</Link>
-            <Link to="/about" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> About me</Link>
-            <a href="/projedcts" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Projects</a>
+          <h1 className='flex w-auto mx-5 items-center mt-5 '>My life programming</h1>
+          <div className='flex items-center justify-center'>
+            <div className='bg-zinc-800 w-auto h-16 flex items-center text-center gap-4 mt-5 rounded-md px-4 '>
+              <Link to="/home" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Home</Link>
+              <Link to="/about" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md w-28'> About me</Link>
+              <a href="/projects" className='flex items-center cursor-pointer transition px-5 bg-zinc-500 h-10 rounded-md'> Projects</a>
             </div>
             <Outlet />
+          </div>
         </div>
-      </div>
       ) : (
-        <div className='w-full grid grid-cols-3  px-1'>
-          <div> daskjflkjh</div>
+        <div className='w-96 flex px-1'>
+          <div onClick={() => { Menu_click() }} className='fixed z-20 object-cover filter invert'><img src="src/components/media/icons8-menu.svg" alt="" /></div>
+
+          <div className= {`h-full fixed z-10 bg-zinc-700 w-32 ${isOpen ? 'grid grid-rows-12 left-0 ' : 'hidden'}`}>
+            <div className='grid grid-rows-3 row-start-2 gap-7'>
+              <Link to='/home' className='h-3 block  text-center '>home</Link>
+              <Link to='/about' className='h-3 text-center '> about</Link>
+              <Link to='/projects' className='h-3 text-center '> projects</Link>
+            </div>
+
+          </div>
+
           <Outlet />
-      </div>
+        </div>
       )}
     </div>
   )
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,19 +1,23 @@
 import NavBar from '../components/NavBar'
 import Footer from '../components/footer';
-
+import asier_img from './media/DSC_9703.jpeg'
+import GithubLogo from './media/github-mark-white.png'
+import backend from './media/icons8-backend-50.png'
+import ImgDatabase from './media/icons8-database-50.png'
+import ImgReact from './media/React.png'
 const Home = () =>{
     return (
         <div className="h-screen">
             <NavBar />
             
             <div className='flex justify-center'>
-                <div className='bg-zinc-600 flex justify-center items-center w-96 my-9 py-6 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)]'> 
-                    <div className='flex justify-center '>
-                    <img src="src/pages/media/DSC_9703.jpeg" alt="imagen de Asier Alberi" className='h-40 rounded-full'/></div>
+                <div className='bg-zinc-600 flex justify-center items-center lg:w-2/6 my-9 py-6 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)] sm:w-/4'> 
+                    <div className='flex justify-center px-2'>
+                    <img src={asier_img} alt="imagen de Asier Alberi" className='h-40 rounded-full '/></div>
                     <div className='mx-3'>
                     <div className='justify-center text w-15'>Asier Alberdi</div>
                     <div className='w-15 text-zinc-400'>@Txuli</div>
-                    <div className='w-7'><a href="https://github.com/Txuli17" target='blank'><img src="src/pages/media/icons8-github.svg" alt="github" className='h-7' /></a></div>
+                    <div className='w-7'><a href="https://github.com/Txuli17" target='blank'><img src={GithubLogo} alt="github" className='h-7' /></a></div>
                     
                 </div>
             </div> 
@@ -24,22 +28,22 @@ const Home = () =>{
                 <div className='grid sm:grid-cols-3 xl:grid-flow-row mx-3'>
                     <div className='flex justify-center'>
                         <div className='w-3/4 bg-zinc-600 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)] p-2 mx-3  sm: my-6'>
-                            <div className='w-full flex justify-center'><img src="src/pages/media/icons8-backend-50.png" alt="backend image" className=' object-cover filter invert '/></div>
+                            <div className='w-full flex justify-center'><img src={backend} alt="backend image" className=' object-cover filter invert '/></div>
                             <div><h2 className='text-lg w-full flex justify-center'><b>Backend</b></h2></div>
                             <div className='content-center'>I want to learn more about APIS for the requests of the server and make a new project about photography.</div>
                         </div>
                     </div>
                     <div className='flex justify-center'>
                         <div className='w-3/4 bg-zinc-600 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)] p-2 mx-3 my-6'>
-                            <div className='w-full flex justify-center'><img src="src/pages/media/icons8-database-50.png" alt="database image" className=' object-cover filter invert '/></div>
+                            <div className='w-full flex justify-center'><img src={ImgDatabase} alt="database image" className='filter invert '/></div>
                             <div><h2 className='text-lg w-full flex justify-center'><b>Databases</b></h2></div>
                             <div className='content-center'>I want to learn databases to create different parts of my future projects.</div>
                         </div>
                     </div>
                     <div className='flex justify-center'>
                         <div className='w-3/4 bg-zinc-600 rounded-md border-2 border-solid border-zinc-700 drop-shadow-[0_5px_5px_rgb(0,0,5)] p-2 mx-3 my-6'>
-                            <div className='w-full flex justify-center'><img src="src/pages/media/icons8-react-a-javascript-library-for-building-user-interfaces-48.png" alt="react image" className=' object-cover filter invert '/></div>
-                            <div><h2 className='text-lg w-full flex justify-center'><b>Rreact</b></h2></div>
+                            <div className='w-full flex justify-center'><img src= {ImgReact}  className='filter invert'/></div>
+                            <div><h2 className='text-lg w-full flex justify-center'><b>React</b></h2></div>
                             <div className='content-center'>I want to improve my skills in react to improve my webs in the future.</div>
                         </div>
                     </div>
